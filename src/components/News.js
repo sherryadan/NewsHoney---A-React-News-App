eea474f800dd63364abd62e7ceac288b286c551b
Mohammad Adan <adanmohammad80@gmail.com>
2024-07-04 22:22:29 +0500

    React Day 13

--- src/components/News.js
+++ src/components/News.js
@@ -2,22 +2,17 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  constructor() {
-    super();
-    console.log("Hello I am a constructor from News Component");
-
-    this.state = {
-      articles: [],
-      loading: false,
-    };
-  }
   render() {
     return (
       <div className="container my-3">
         <h2>News Honey Top Headlines</h2>
         <div className="row">
           <div className="col md-3 my-4">
-            <NewsItem title="mytitle" description="mydesc" />
+            <NewsItem
+              title="mytitle"
+              description="mydesc"
+              imageUrl="https://media.bleacherreport.com/image/upload/c_fill,g_faces,w_3800,h_2000,q_95/v1719939575/aolsemphnbubil6dqqu1.jpg"
+            />
           </div>
           <div className="col md-3 my-4">
             <NewsItem title="mytitle" description="mydesc" />
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-  constructor() {
-    super();
-    console.log("Hello I am a constructor from NewsItem");
-  }
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
